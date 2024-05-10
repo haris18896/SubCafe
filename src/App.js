bb936e6e8bbc770db42a5de8c7152bf8a92ca21c
haris18896 <haris18896@gmail.com>
2024-05-10 17:49:59 +0500

    working with subcafe

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import {SafeAreaView, Text, View} from 'react-native';
+
+// ** Third Party Packages
+import Toast from 'react-native-toast-message';
+import {PaperProvider} from 'react-native-paper';
+import {ThemeProvider} from 'styled-components';
+import {StripeProvider} from '@stripe/stripe-react-native';
+
+// ** Store
+import {Provider} from 'react-redux';
+import {store, persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+
+// ** Custom Components
+import MainStack from './navigation';
+import {theme as AppTheme} from './@core/infrustructure/theme';
 
 function App() {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>This is SubCafe App</Text>
-      </View>
-    </SafeAreaView>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={AppTheme}>
+          <PaperProvider>
+            <StripeProvider publishableKey={STRIPE_KEY}>
+              <MainStack />
+            </StripeProvider>
+            <Toast />
+          </PaperProvider>
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
   );
 }
 
